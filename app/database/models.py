db332d8f0de6478470e9a1b652203e451e164bdb
Shivansh Maurya <shivanshindia001@gmail.com>
2026-08-09 01:17:28 +0530

    seting up database  and document model

---
+++ app/database/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, Integer, String, DateTime
+from app.database.database import Base
+
+
+class Document(Base):
+    __tablename__ = "documents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    file_id = Column(String, unique=True, nullable=False)
+    filename = Column(String, nullable=False)
+    upload_date = Column(
+        DateTime,
+        default=lambda: datetime.now(timezone.utc)
+    )
+    page_count = Column(Integer, nullable=True)
