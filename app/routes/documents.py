74067bbbf0a4d185069db8d945e48c2a35584038
Shivansh Maurya <shivanshindia001@gmail.com>
2026-08-11 00:16:25 +0530

    only pdf input with particular size

--- app/routes/documents.py
+++ app/routes/documents.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from pathlib import Path
 import uuid
 
@@ -12,18 +12,31 @@ UPLOAD_DIR.mkdir(exist_ok=True)
 
 @router.post("/upload")
 def upload_pdf(file: UploadFile = File(...)):
+
+    # Check file extension
+    file_extension = Path(file.filename).suffix.lower()
+
+    if file_extension != ".pdf":
+        raise HTTPException(
+            status_code=400,
+            detail="Only PDF files are allowed"
+        )
+
+    # Generate unique ID of pdf file jo upload hua ha 
     file_id = uuid.uuid4()
 
-    file_extension = Path(file.filename).suffix
+    # Create unique filename of file 
+    unique_filename = f"{file_id}.pdf"
 
-    unique_filename = f"{file_id}{file_extension}"
-
+    # Create file path and yahan pe file ko save karenge uploads folder me or
+    #  upload route me file ko save karenge uploads folder me
     file_path = UPLOAD_DIR / unique_filename
 
+    # Save PDF to the uploads directory so ham use access kar paye or usko read kar sake 
     with open(file_path, "wb") as buffer:
         buffer.write(file.file.read())
 
-    return {
+    return {  # normal response ha file hone ke baad uske barre me
         "file_id": str(file_id),
         "original_filename": file.filename,
         "saved_filename": unique_filename,
