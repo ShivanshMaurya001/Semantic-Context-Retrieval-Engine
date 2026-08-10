e691a493e7dc26d2bdfad701d437f4b8d30aecea
Shivansh Maurya <shivanshindia001@gmail.com>
2026-08-10 23:39:32 +0530

    add document upload setup

--- app/database/models.py
+++ app/database/models.py
@@ -10,8 +10,12 @@ class Document(Base):
     id = Column(Integer, primary_key=True, index=True)
     file_id = Column(String, unique=True, nullable=False)
     filename = Column(String, nullable=False)
+
     upload_date = Column(
         DateTime,
         default=lambda: datetime.now(timezone.utc)
     )
+
     page_count = Column(Integer, nullable=True)
+
+    status = Column(String, default="uploaded", nullable=False)
